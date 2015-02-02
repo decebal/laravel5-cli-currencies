1f50322ab997f2a36058c74d240782037fb0b8f6
Decebal D <decebal1988@gmail.com>
2015-02-02 09:36:38 +0200

    wrap up

--- app/Console/Commands/GenerateReport.php
+++ app/Console/Commands/GenerateReport.php
@@ -1,75 +1,81 @@
 <?php namespace App\Console\Commands;
 
-use App\Services\ReadCsv;
+use App\Contracts\CurrencyConverter;
+use App\Models\Merchant;
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
-class GenerateReport extends Command {
+/**
+ * Class GenerateReport
+ *
+ * @package ReportDemo\Console\Commands
+ */
+class GenerateReport extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'report';
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'report';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates sales report for vendor.';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Generates sales report for vendor.';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $currencyConverter = new CurrencyConverter(
+            $this->option('currency'),
+            $this->option('advanced-detect')
+        );
+        $merchant = new Merchant($currencyConverter);
+        list($headers, $rows) = $merchant->getTransactionsById($this->argument('vendor-id'));
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire(Filesystem $filesystem)
-	{
-		ReadCsv::loadFromCsv($filesystem);
-		//$this->argument('vendorId')
-		//$this->option('currency')
-		//get vendor model
-//		$headers = $this->vendor->getHeaders();
-//		$rows = getData($this->vendorId);
-//		$this->table($headers, $rows, 0);
-	}
+        $this->table($headers, $rows);
+    }
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return [
-			['vendorId', InputArgument::REQUIRED, 'Id of desired vendor; Check the vendors with the command show:vendors'],
-		];
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['vendor-id', InputArgument::REQUIRED, 'Id of desired vendor'],
+        ];
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return [
-			['currency', 'c', InputOption::VALUE_OPTIONAL, 'International Short, defaults to GBR', 'GBR'],
-		];
-	}
-
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['currency', 'c', InputOption::VALUE_OPTIONAL, 'International Short, defaults to GBP; Available at this time: GBP, USD, EUR', 'GBP'],
+            ['advanced-detect', 'ad', InputOption::VALUE_OPTIONAL, 'Advanced Currency Detection', 0],  //this uses the regex engine, so it is not recommended for large data
+        ];
+    }
 }
--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -1,9 +1,97 @@
-<?php
+<?php namespace App\Models;
 
+use App\Contracts\CurrencyConverter;
+use League\Csv\Reader;
+
+/**
+ * Class Merchant
+ */
 class Merchant
 {
-    public function getTransactions()
+    /**
+     * dummy file path with data
+     *
+     * @var string
+     */
+    private $filename = '';
+
+    /**
+     * Csv Reader Instance
+     *
+     * @var Reader
+     */
+    private $inputCsv = null;
+
+    /**
+     * @var CurrencyConverter
+     */
+    private $currencyConverter = null;
+
+    /**
+     * Init Merchant Details
+     *
+     * @param CurrencyConverter $currencyConverter
+     */
+    public function __construct(CurrencyConverter $currencyConverter)
     {
-        
+        $this->currencyConverter = $currencyConverter;
+        $this->filename = base_path() . '/storage/files/data.csv';
+        $this->loadData();
+    }
+
+    /**
+     * initialize csv reader
+     */
+    public function loadData()
+    {
+        $inputCsv = Reader::createFromPath(new \SplFileObject($this->filename));
+        $inputCsv->setDelimiter(';');
+        $inputCsv->setEncodingFrom("iso-8859-15");
+        $this->inputCsv = $inputCsv;
+    }
+
+    /**
+     * @param $vendorId
+     * @return array
+     */
+    public function getTransactionsById($vendorId)
+    {
+        $headers = $this->inputCsv->fetchOne(0);
+        $contents = $this->inputCsv
+            ->addFilter(function (
+                $row,
+                $index
+            ) {
+                return $index > 0; //we don't take into account the header
+            })
+            ->addFilter(function ($row) {
+                return isset($row[0], $row[1], $row[2]); //we make sure the data are present
+            })
+            ->addFilter(function ($row) use ($vendorId) {
+                return $row[0] == $vendorId; //return transaction data by vendor
+            })
+            ->fetchAssoc();
+
+        $contents = $this->convertValues($contents);
+
+        return array(
+            $headers,
+            $contents
+        );
+    }
+
+    /**
+     * Use CurrencyConverter to decide and convert currency values
+     *
+     * @param $transactions array
+     * @return mixed
+     */
+    private function convertValues($transactions)
+    {
+        foreach ($transactions as &$transaction) {
+            $transaction['value'] = $this->currencyConverter->convertCurrency($transaction['value']);
+        }
+
+        return $transactions;
     }
 }
